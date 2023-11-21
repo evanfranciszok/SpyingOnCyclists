0349228d9985606e31bddf810c111d0720079eae
Evan Franciszok <git@evan.mailtje.net>
2023-11-21 14:02:30 +0100

    First Commit

---
+++ DisseminationAlg.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import optparse
+import random
+
+
+
+# we need to import some python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+from sumolib import checkBinary  # Checks for the binary in environ vars
+import traci
+
+
+def get_options():
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = opt_parser.parse_args()
+    return options
+
+
+# contains TraCI control loop
+def run():
+    step = 0
+    RoadEdgeValues = assignValuesToRoadEdges()
+    print(RoadEdgeValues)
+    while traci.simulation.getMinExpectedNumber() > 0: # type: ignore
+        traci.simulationStep()
+        print(str(vehiclesInDisseminationDistance()))
+        # print(step)
+        step += 1
+
+    traci.close()
+    sys.stdout.flush()
+
+
+def vehiclesInDisseminationDistance():
+    inDistance = []
+    vehicleList = traci.vehicle.getIDList()
+    CurrentNoOfVehicle = 0; 
+    for vehID in vehicleList:
+        CurrentNoOfVehicle+=1
+        ownPos = traci.vehicle.getPosition(vehID)
+        for otherID in range(len(vehicleList)-CurrentNoOfVehicle):
+            vehIDOther = vehicleList[otherID+CurrentNoOfVehicle]
+            otherPos = traci.vehicle.getPosition(vehIDOther)
+            dist = traci.simulation.getDistance2D(ownPos[0], ownPos[1], otherPos[0], otherPos[1], False, False)
+            if(dist < 25.0):
+                # print(str(round(dist)) + "m for " + str(vehID) + " and " + str(vehIDOther))
+                inDistance.append(tuple((str(vehID), str(vehIDOther))))
+    return inDistance
+
+def assignValuesToRoadEdges():
+    roadEdges = {}
+    random.seed(10) #make sure the random numbers are the same everytime. Remove this if this is not wanted behaviour
+    for roadId in traci.edge.getIDList():
+        roadEdges[roadId] = random.randint(1,9)#value of 1 to (and including) 9 to simulate the road score
+    return roadEdges
+
+# main entry point
+if __name__ == "__main__":
+    options = get_options()
+
+    # check binary
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # traci starts sumo as a subprocess and then this script connects and runs
+    traci.start([sumoBinary, "-c", "DualRoad.sumocfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+    run()
