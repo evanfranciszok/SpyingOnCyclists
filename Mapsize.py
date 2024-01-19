17a386372cbed4db531dc85a4160fa3e2bfdf2e7
yayayara <yaravanschendel@gmail.com>
2024-01-19 16:47:12 +0100

    Change enum to all mapsizes

--- Mapsize.py
+++ Mapsize.py
@@ -3,5 +3,6 @@ from enum import Enum
 class Mapsize(Enum):
 # Percentage of data disseminated from its own collected data
     SMALL = 1,
-    MEDIUMSMALL = 2,
-    # MEDIUM = 3
+    SMALL2 = 2,
+    MEDIUM = 3, 
+    LARGE = 4
