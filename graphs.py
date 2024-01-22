3d36b698c25dd656cae8da638174bdbe9a8f8e35
yayayara <yaravanschendel@gmail.com>
2024-01-22 14:13:55 +0100

    remove the decimal from the bars

--- graphs.py
+++ graphs.py
@@ -12,33 +12,6 @@ grouped_data = df.groupby(['name of dissemination case', 'number of bikes'])
 # Create a new DataFrame to store the average duration for each case and number of bikes
 average_duration_df = grouped_data['duration'].mean().reset_index()
 
-# # Create a new column 'undetermined' with value 43200 for the case 'undetermined'
-# average_duration_df.loc[average_duration_df['name of dissemination case'] == 'undetermined', 'duration'] = 43200
-
-# # Create a bar plot
-# fig, ax = plt.subplots(figsize=(10, 6))
-
-# # Plotting bars for each case
-# for name, group in average_duration_df.groupby('name of dissemination case'):
-#     ax.bar(group['number of bikes'], group['duration'], label=name)
-
-# # Set y-axis range
-# ax.set_ylim(0, 43200)
-
-# # Add light grey bar for 'undetermined'
-# undetermined_group = average_duration_df[average_duration_df['name of dissemination case'] == 'undetermined']
-# ax.bar(undetermined_group['number of bikes'], undetermined_group['duration'],
-#        color='lightgrey', label='undetermined')
-
-# # Set labels and title
-# ax.set_xlabel('Number of Bikes')
-# ax.set_ylabel('Average Duration')
-# ax.set_title('Average Duration for Different Cases and Number of Bikes')
-# ax.legend()
-
-# # Show the plot
-# plt.show()
-
 # Set up a dictionary to store individual axes for each case
 axes_dict = {}
 
@@ -52,7 +25,7 @@ for name, group in average_duration_df.groupby('name of dissemination case'):
     bars = ax.bar(group['number of bikes'], group['duration'], label=name)
 
     # Set y-axis range
-    ax.set_ylim(0, 43200)
+    ax.set_ylim(0, 45000) # Adjusted range for better visualization
 
     # Set labels and title
     ax.set_xlabel('Number of Bikes')
@@ -63,7 +36,7 @@ for name, group in average_duration_df.groupby('name of dissemination case'):
     # Annotate average duration above each bar
     for bar in bars:
         height = bar.get_height()
-        ax.annotate(f'{height:.2f}', xy=(bar.get_x() + bar.get_width() / 2, height),
+        ax.annotate(f'{int(height)}', xy=(bar.get_x() + bar.get_width() / 2, height),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
                     ha='center', va='bottom', rotation='vertical')
